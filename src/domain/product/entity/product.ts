dc1bfbc1859e6ec012ba4a71174ca9d5b1183251
Gabriel Padilha <gabriel.padilha@brasilfone.com.br>
2022-06-23 22:30:20 -0300

    Product Notifications

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,29 +1,44 @@
+import { Entity } from "../../@shared/entity/entity.abstract";
+import { NotificationError } from "../../@shared/notification/notification.error";
 import { IProduct } from "./product.interface";
 
-export class Product implements IProduct {
-  private _id: string;
+export class Product extends Entity implements IProduct {
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
 
     this.validate();
+
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
+    }
   }
 
   validate(): boolean {
     if (!this._id || this._id.length === 0) {
-      throw new Error("Id is required");
+      this.notification.addError({
+        message: "Id is required",
+        context: "product",
+      });
     }
 
     if (!this._name || this._name.length === 0) {
-      throw new Error("Name is required");
+      this.notification.addError({
+        message: "Name is required",
+        context: "product",
+      });
     }
 
     if (!this._price || this._price < 0) {
-      throw new Error("Price must be greater than zero");
+      this.notification.addError({
+        message: "Price must be greater than zero",
+        context: "product",
+      });
     }
 
     return true;
